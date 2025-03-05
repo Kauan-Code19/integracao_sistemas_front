ea884817dc794534f024726d8355661783856ca9
Kauan-Code19 <pkauprofissional@gmail.com>
2025-03-05 11:35:31 -0300

    ajuste na estilização da view login adicionando uma div para imagem, ajuste na paleta de cor, font-family e estilização da mensagem de erro.

--- app/Views/autenticacao/login.php
+++ app/Views/autenticacao/login.php
@@ -7,26 +7,33 @@
     <title>Traduzzo</title>
     <link rel="stylesheet" href="<?= base_url('assets/css/reset.css') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/autenticacao/login.css') ?>">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <div class="container">
-        <?php if (session()->getFlashdata('error')) : ?>
-            <div class="error-message">
-                <?= session()->getFlashdata('error'); ?>
-            </div>
-        <?php endif; ?>
+        <div class="login">
+            <?php if (session()->getFlashdata('error')) : ?>
+                <div class="error-message">
+                    <?= session()->getFlashdata('error'); ?>
+                </div>
+            <?php endif; ?>
 
-        <h2>Login</h2>
-        <form action="<?= base_url('login') ?>" method="POST">
-            <label for="email">E-mail</label>
-            <input type="email" id="email" name="email" required>
+            <h2>Login</h2>
+            <form action="<?= base_url('login') ?>" method="POST">
+                <label for="email">E-mail</label>
+                <input type="email" id="email" name="email" required>
 
-            <label for="senha">Senha</label>
-            <input type="password" id="senha" name="senha" required>
+                <label for="senha">Senha</label>
+                <input type="password" id="senha" name="senha" required>
 
-            <button type="submit" class="buttonEntrar">Entrar</button>
-        </form>
+                <button type="submit" class="buttonEntrar">Entrar</button>
+            </form>
+        </div>
+
+        <div class="imagem">
+            <img src="<?= base_url('assets/traduzzo_cover.jpeg'); ?>" alt="Logo">
+        </div>
     </div>
 </body>
 
