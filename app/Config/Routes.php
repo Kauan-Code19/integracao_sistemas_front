cb77b2fb35b1de98f9fbce893d0c51b8bc4b362c
Kauan-Code19 <pkauprofissional@gmail.com>
2025-03-17 16:16:29 -0300

    Projeto criado para fazer simulção de tradução mas houve mudança de planos para permanecer apenas como gestao de usuarios em questao do tempo.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,7 +11,6 @@ $routes->get('/user_edit/(:num)', 'UserController::userEditView/$1');
 $routes->get('/user_details/(:num)', 'UserController::userDetailsView/$1');
 $routes->get('/user_registration', 'UserController::userRegistrationView');
 $routes->get('/list_users', 'UserController::listUsers');
-$routes->get('/client_list', 'ClientController::clientListView');
 
 $routes->post('/login', 'LoginController::logar');
 $routes->post('/register_user', 'UserController::registerUser');
--- app/Views/includes/sidebar.php
+++ app/Views/includes/sidebar.php
@@ -1,10 +1,6 @@
 <div id="sidebar" class="sidebar">
     <ul>
         <li><a href="<?= base_url('user_list') ?>">Usuários</a></li>
-        <li><a href="<?= base_url('client_list') ?>">Clientes</a></li>
-        <li><a href="<?= base_url('translator_list') ?>">Tradutores</a></li>
-        <li><a href="<?= base_url('budget_list') ?>">Orçamento</a></li>
-        <li><a href="<?= base_url('financial_list') ?>">Financeiro</a></li>
     </ul>
 </div>
 
